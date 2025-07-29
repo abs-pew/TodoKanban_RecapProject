0809ff3cea455bf4f7d016a52f5c3debd36cf223
abdulsalam <abs.pew@gmail.com>
2025-07-29 13:43:46 +0200

    Updated -  all files

--- java/de/neuefische/todokanban_recapproject/controller/TodoController.java
+++ java/de/neuefische/todokanban_recapproject/controller/TodoController.java
@@ -1,5 +1,7 @@
 package de.neuefische.todokanban_recapproject.controller;
 
+import de.neuefische.todokanban_recapproject.dto.TodoInputDto;
+import de.neuefische.todokanban_recapproject.exceptions.TodoNotFoundException;
 import de.neuefische.todokanban_recapproject.model.Todo;
 import de.neuefische.todokanban_recapproject.repository.TodoRepository;
 import de.neuefische.todokanban_recapproject.service.TodoService;
@@ -23,23 +25,23 @@ public TodoController(TodoService todoService) {
 }
 
 @PostMapping
-    public Todo addTodo(@RequestBody Todo newTodo) {
-    return todoService.addTodo(newTodo);
+    public Todo addTodo(@RequestBody TodoInputDto newInputDto) {
+    return todoService.addTodo(newInputDto);
 }
 
 @GetMapping("/{id}")
-    public Todo getTodoById(@PathVariable String id) {
+    public Todo getTodoById(@PathVariable String id){
     return todoService.getTodoById(id);
 }
 
 
     @PutMapping("/{id}")
-    public Todo updateTodoById(@PathVariable String id, @RequestBody Todo toBeUpdatedTodo) {
+    public Todo updateTodoById(@PathVariable String id, @RequestBody Todo toBeUpdatedTodo) throws TodoNotFoundException {
         return todoService.updateTodoById(id, toBeUpdatedTodo);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTodoById(@PathVariable String id) {
+    public void deleteTodoById(@PathVariable String id) throws TodoNotFoundException {
         todoService.deleteTodoById(id);
     }
 }
--- java/de/neuefische/todokanban_recapproject/service/TodoService.java
+++ java/de/neuefische/todokanban_recapproject/service/TodoService.java
@@ -1,11 +1,14 @@
 package de.neuefische.todokanban_recapproject.service;
 
+import de.neuefische.todokanban_recapproject.dto.TodoInputDto;
+import de.neuefische.todokanban_recapproject.exceptions.TodoNotFoundException;
 import de.neuefische.todokanban_recapproject.model.Todo;
 import de.neuefische.todokanban_recapproject.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class TodoService {
@@ -19,7 +22,9 @@ public class TodoService {
         return todoRepo.findAll();
     }
 
-    public Todo addTodo(Todo newTodo) {
+    public Todo addTodo(TodoInputDto newInputDTO) {
+        long idCounter = (Long) todoRepo.count() + 1;
+        Todo newTodo = new Todo( Long.toString(idCounter), newInputDTO.description(), "OPEN");
         todoRepo.save(newTodo);
         return newTodo;
     }
@@ -27,25 +32,25 @@ public class TodoService {
     public Todo getTodoById(String id) {
         return todoRepo
                 .findById(id)
-                .orElse(new Todo("UKID", "Unknown ID. No data found.", "Unknown"));
+         .orElseThrow(() -> new TodoNotFoundException("Todo item with the id: " + id + " not found!"));
     }
 
     public Todo updateTodoById(String id, Todo toBeUpdated) {
         Todo existingTodo;
         existingTodo = getTodoById(id);
-        if (existingTodo.id() != "UKID") {
-            existingTodo = toBeUpdated;
-            return todoRepo.save(existingTodo);
-        }
-return new Todo("UKID", "Unknown ID. No data found.", "Unknown");
+        existingTodo = toBeUpdated;
+     return todoRepo.save(existingTodo);
+
+//        if (existingTodo.id() != "UKID") {
+//            existingTodo = toBeUpdated;
+//            return todoRepo.save(existingTodo);
+//        }
+//return new Todo("UKID", "Unknown ID. No data found.", "Unknown");
 
     }
 
     public void deleteTodoById(String id) {
-        Todo existingTodo;
-        existingTodo = getTodoById(id);
-        if (existingTodo.id() != "UKID") {
-            todoRepo.delete(existingTodo);
-        }
+            todoRepo.deleteById(id);
     }
+
 }
